3a80a5f045cf928a17fde664cffbc68210386bd1
Cole Leahy <coleleahy@gmail.com>
2026-04-05 18:17:38 -0700

    WIP

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -51,9 +51,8 @@ object Par {
   // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
   def unit[A](a: A): Par[A] = _ => UnitFuture(a)
 
-  def fork[A](
-    a: => Par[A]
-  ): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
+  // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
+  def fork[A](a: => Par[A]): Par[A] =
     es =>
       es.submit {
         new Callable[A] {
@@ -195,7 +194,6 @@ object Par {
   }
 
   /*
-  map(y)(g) == map(y)(g)                      // Identity.
   map(map(y)(g))(id) == map(y)(g)             // By law "map(y)(id) == y", taking map(y)(g) as "y".
   map(map(y)(g))(id) == map(y)(id compose g)  // Because id compose g is just g.
   map(map(y)(g))(f) == map(y)(f compose g)    // By abstracting away from "id" on the previous line. Why can we do this? Because map can't possibly behave differently for "id" than for any other function "f", since map[A, B] is parametrically polymorphic (which means it couldn't possibly follow different code paths based on the details of the function of type A => B that's passed in).
